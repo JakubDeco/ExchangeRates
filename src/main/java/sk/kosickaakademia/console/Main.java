6495c473abc7311f41c810c04d5b00826c0f34b9
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-04-13 10:06:05 +0200

    refactoring Main.java and added new Main class used for gui version

--- src/main/java/sk/kosickaakademia/console/Main.java
+++ src/main/java/sk/kosickaakademia/console/Main.java
@@ -1,11 +1,9 @@
-package sk.kosickaakademia;
+package sk.kosickaakademia.console;
 
 import sk.kosickaakademia.calc.Convert;
 
-
-public class Main
-{
-    public static void main( String[] args ) {
+public class Main {
+    public static void main(String[] args) {
         System.out.println( "Hello World!" );
         Convert convert = new Convert("EUR");
         convert.convert(10);
